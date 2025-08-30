3453792742efffe8f5798d7d6c8f1391c26845dc
Ayesha-Jan <ayeshajan667@gmail.com>
2025-08-30 16:08:38 +0200

    Ordered files

--- task5_6/main.py
+++ task5_6/main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-# Example: load your two datasets
-df1 = pd.read_csv("data/ev_sales.csv")  # could have ISO codes
-df2 = pd.read_csv("data/energy_emissions.csv")  # lacks ISO codes
+# load your two datasets
+df1 = pd.read_csv("data/ev_sales.csv")
+df2 = pd.read_csv("data/energy_emissions.csv") 
 
-df1.columns = df1.columns.str.strip()  # remove leading/trailing spaces
+df1.columns = df1.columns.str.strip()
 
 print([repr(c) for c in df1.columns])
 
@@ -16,15 +16,13 @@ print(df2.head())
 print(df2.columns)
 print(df2.isnull().sum())
 
-# Remove leading/trailing spaces and lowercase
 df1['country'] = df1['Entity'].str.strip().str.lower()
 df2['country'] = df2['country'].str.strip().str.lower()
 
 df1['Year'] = df1['Year'].astype(int)
 df2['Year'] = df2['Year'].astype(int)
 
-# Drop unnecessary columns from df1
 df1 = df1.drop(columns=['Entity', 'Code'])
 
-merged_df = pd.merge(df1, df2, on=['country', 'Year'], how="outer")  # 'outer' to keep all countries
+merged_df = pd.merge(df1, df2, on=['country', 'Year'], how="outer")
 merged_df.to_csv("merged_data.csv", index=False)
